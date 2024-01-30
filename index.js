052abdcf140f6aabf39c5ddc290785ffe78c4c5e
Brendan McGinty <brendan.mcginty14@gmail.com>
2024-01-30 17:55:39 -0500

    completed the function by defining the content of SVG file as variable and called fs function to write new file

---
+++ index.js
@@ -0,0 +1,54 @@
+// import functionality for express, fs, and inquirer (node modules are installed)
+const fs = require('fs');
+const inquirer = require('inquirer');
+const http = require('http');
+
+// create function which starts with an inquirer prompt to gather user input for the three letters, shape / text color as well as shape of the logo
+async function createLogo() {
+    try {
+        const answers = await inquirer.prompt([
+            {
+                name: 'text',
+                message: 'Enter up to three characters:',
+                validate: function (input) {
+                    return input.length <= 3 ? true : 'Please enter up to three characters.'; // Ensure that the input for the letters contains more than three letters
+                }
+            },
+            {
+                name: 'textColor',
+                message: 'Enter text color (color keyword or hexadecimal number):'
+            },
+            {
+                name: 'shape',
+                message: 'Choose a shape:',
+                type: 'list',
+                choices: ['circle', 'triangle', 'square']
+            },
+            {
+                name: 'shapeColor',
+                message: 'Enter shape color (color keyword or hexadecimal number):'
+            }
+        ]);
+
+        const { text, textColor, shape, shapeColor } = answers;
+        const svgContent = `<?xml version="1.0" standalone="no"?>
+    <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 20010904//EN"
+     "http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd">
+    <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+      <rect width="100%" height="100%" fill="${shapeColor}" />
+      <text x="50%" y="50%" text-anchor="middle" fill="${textColor}" font-size="48">${text}</text>
+    </svg>
+    `;
+
+        fs.writeFileSync('logo.svg', svgContent);
+        console.log("Generated logo.svg");
+
+    } catch (err) {
+        console.error("Error:", err);
+    }
+}
+
+// Call the function to create the logo and start the server
+createLogo();
+
+
